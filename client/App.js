a166ceadbc64ae14f3e796841e765e0df79a2c75
permeg <pmegan883@gmail.com>
2026-01-27 17:48:46 -0800

    Updating the dependencies to be in accordance with expo requirements.

--- client/App.js
+++ client/App.js
@@ -1,14 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import SomeComponent from './src/SomeComponent';
+import { StyleSheet, View } from 'react-native';
+import WelcomeScreen from './src/pages/WelcomeScreen';
+import LogInScreen from './src/pages/LogInScreen';
+import SignUpScreen from './src/pages/SignUpScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is me testing how to use Expo Go!</Text>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="LogInScreen" component={LogInScreen} />
+        <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+      </Stack.Navigator>
       <StatusBar style="auto" />
-      <SomeComponent />
-    </View>
+    </NavigationContainer>
   );
 }
 
@@ -16,7 +28,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+
+
