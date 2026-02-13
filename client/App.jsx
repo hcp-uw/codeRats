4e3e9df9339a6ec65776239b8d19c1d0d044e2d7
permeg <pmegan883@gmail.com>
2026-02-12 18:28:36 -0800

    Edits to App.jsx

---
+++ client/App.jsx
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import WelcomeScreen from './src/WelcomeScreen';
+import LogInScreen from './src/LogInScreen';
+import SignUpScreen from './src/SignUpScreen';
+import Profile from './src/Profile';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="LogInScreen" component={LogInScreen} />
+        <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+        <Stack.Screen name="Profile" component={Profile} />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
